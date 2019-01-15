3464f0f594dd9c33b345a614184cdf0df87572e2
Julia <jkmcgeoghan@myseneca.ca>
2019-01-15 00:14:55 -0500

    Add routing to auth pages

--- ClientApp/src/app/features/auth/pages/login/login.component.ts
+++ ClientApp/src/app/features/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+  }
+
+  login() {
+    this.router.navigate(['dashboard']);
+  }
+
+  resetPassword() {
+    this.router.navigate(['auth/passwordReset']);
   }
 
 }
--- ClientApp/src/app/features/auth/pages/password-reset/password-reset.component.ts
+++ ClientApp/src/app/features/auth/pages/password-reset/password-reset.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-password-reset',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordResetComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['auth']);
   }
 
 }
