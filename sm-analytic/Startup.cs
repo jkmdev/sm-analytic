1ac3af5e42a3b03091bd0af9aaa749edbe424617
Julia McGeoghan <jkmcgeoghan@myseneca.ca>
2019-01-29 00:49:40 -0500

    Deployment config

--- sm-analytic/Startup.cs
+++ sm-analytic/Startup.cs
@@ -27,6 +27,12 @@ namespace sm_analytic
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AutomaticAuthentication = false;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
