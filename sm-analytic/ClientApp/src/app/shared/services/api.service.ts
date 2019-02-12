929d4d27d3837504b11dfc279101c8fdc74d6b00
Julia McGeoghan <jkmcgeoghan@myseneca.ca>
2019-02-12 17:10:14 -0500

    Add angular api service file to project

---
+++ sm-analytic/ClientApp/src/app/shared/services/api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { _throw as throwError } from 'rxjs/observable/throw';
+import { environment } from '../../../environments/environment';
+
+@Injectable()
+export class ApiService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private formatErrors(error: any) {
+    return throwError(error.error);
+  }
+
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http.get(`${environment.api_url}${path}`, { responseType: 'text' });
+      // .pipe(catchError(this.formatErrors));
+  }
+
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http.post(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body)
+    );
+    //.pipe(catchError(this.formatErrors));
+  }
+
+}
