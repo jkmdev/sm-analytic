1cae8bf4198323115e6b93eea5ad25d3a8b6ccc7
Julia McGeoghan <jkmcgeoghan@myseneca.ca>
2019-03-19 18:39:00 -0400

    init empty chart objects

--- sm-analytic/ClientApp/src/app/features/dashboard/components/charts/base-chart/base-chart.component.ts
+++ sm-analytic/ClientApp/src/app/features/dashboard/components/charts/base-chart/base-chart.component.ts
@@ -40,8 +40,6 @@ export class BaseChartComponent {
 
       var hasData = true;
 
-      console.log(ele.data);
-
       if (ele.data.length == 0) {
         hasData = false;
       } else {
--- sm-analytic/ClientApp/src/app/features/dashboard/pages/follower/follower.component.ts
+++ sm-analytic/ClientApp/src/app/features/dashboard/pages/follower/follower.component.ts
@@ -4,6 +4,14 @@ import { FollowersService } from 'app/shared/services/followers.service';
 import { TwitterDataService } from 'app/shared/services/twitter-data.service';
 import { Subscription } from 'rxjs/Subscription';
 
+interface Chart {
+  title: string,
+  subTitle: string,
+  chartLabels: object,
+  chartData: Array<object>,
+  chartType: string
+}
+
 @Component({
   selector: 'app-follower',
   templateUrl: './follower.component.html',
@@ -11,105 +19,52 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class FollowerComponent implements OnInit {
 
-  tweets: any;
-  followers: any;
-  engagementByHourData: any;
-  engagementByDayData: any;
-  engagementTotal: any;
-  followerJoinedAt: any;
+  private engagementByHourData: any;
+  private engagementByDayData: any;
+  private engagementTotal: any;
+  private followerJoinedAt: any;
   private twitterDataUpdateRef: Subscription = null;
 
   constructor(
     private engagementService: EngagementService,
     private twitterDataService: TwitterDataService,
     private followersService: FollowersService
-  ) { }
+  ) {
 
-  ngOnInit() {
+    var chartObject = {
+      title: '',
+      subTitle: '',
+      chartLabels: {},
+      chartData: [],
+      chartType: ''
+    };
+
+    this.engagementByHourData = Object.create(chartObject);
+    this.engagementByDayData = Object.create(chartObject);
+    this.engagementTotal = Object.create(chartObject);
+    this.followerJoinedAt = Object.create(chartObject);
 
     this.drawCharts();
+
+  }
+
+  ngOnInit() {
 
     this.twitterDataUpdateRef = this.twitterDataService.updated.subscribe(() => {
       this.drawCharts();
     });
-  
+
   }
 
   drawCharts() {
-    this.tweets = this.twitterDataService.tweets;
-    this.followers = this.twitterDataService.followers;
-    this.drawEngagementByHour();
-    this.drawEngagementByDay();
-    this.drawEngagementTotal();
-    this.followerJoinedAt = this.followersService.followerJoinedAtData(this.followers);
-  }
 
-  drawEngagementByHour() {
+    const tweets = this.twitterDataService.tweets;
+    const followers = this.twitterDataService.followers;
 
-    var chartLabels = {};
-
-    for (var i = 0; i < 24; i++) {
-      chartLabels[i] = `${i}:00`;
-    }
-
-    var chartData = this.tweets ? this.engagementService.calcEngagementByHour(this.tweets) : [];
-
-    /*chartData.forEach((entry : any) => {
-      entry.fillColor = "rgba(151,249,190,0.5)",
-      entry.strokeColor = "rgba(255,255,255,1)",
-      entry.pointColor = "rgba(220,220,220,1)",
-      entry.pointStrokeColor = "#fff"
-    });
-
-    console.log(chartData);*/
-
-    this.engagementByHourData = {
-      'title': "Posting Times vs. Engagement (Hourly)",
-      'subTitle': "Shows a relationship between when a tweet was posted and how many likes, etc it has for a given hour.",
-      'chartLabels': Object.values(chartLabels),
-      'chartData': chartData
-    };
-
-  }
-
-  drawEngagementByDay() {
-
-    var chartLabels = {
-      0: "Monday",
-      1: "Tuesday",
-      2: "Wednesday",
-      3: "Thursday",
-      4: "Friday",
-      5: "Saturday",
-      6: "Sunday"
-    };
-
-    var chartData = this.tweets ? this.engagementService.calcEngagementByDay(this.tweets) : [];
-
-    this.engagementByDayData = {
-      'title': "Posting Times vs. Engagement (Daily)",
-      'subTitle': "Shows a relationship between when a tweet was posted and how many likes, etc it has for a given day.",
-      'chartLabels': Object.values(chartLabels),
-      'chartData': chartData
-    };
-
-  }
-
-  drawEngagementTotal() {
-
-    var chartLabels = {
-      0: "Tweets",
-      1: "Retweets"
-    };
-
-    var chartData = this.tweets ? this.engagementService.calcEngagementTotal(this.tweets) : [];
-
-    this.engagementTotal = {
-      'title': "Total Engagement",
-      'subTitle': "",
-      'chartLabels': Object.values(chartLabels),
-      'chartData': chartData
-    };
+    this.engagementByHourData = this.engagementService.engagementByHourData(tweets);
+    this.engagementByDayData = this.engagementService.engagementByDayData(tweets);
+    this.engagementTotal = this.engagementService.engagementTotalData(tweets);
+    this.followerJoinedAt = this.followersService.followerJoinedAtData(followers);
 
   }
 
