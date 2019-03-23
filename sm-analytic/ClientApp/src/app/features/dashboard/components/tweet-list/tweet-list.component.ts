36190998eb9edf482c6c876010fc0555f860e2de
Julia McGeoghan <jkmcgeoghan@myseneca.ca>
2019-03-23 17:14:27 -0400

    Finish tweet-list component

--- sm-analytic/ClientApp/src/app/features/dashboard/components/tweet-list/tweet-list.component.ts
+++ sm-analytic/ClientApp/src/app/features/dashboard/components/tweet-list/tweet-list.component.ts
@@ -17,6 +17,7 @@ export class TweetListComponent implements OnInit {
 
     this.tweets = [
       { text: "Doodlee doo", score: 45, user: "Bob", handle: "bobboy", link: "https://twitter.com/jack/status/20", date: "March 21, 2006" },
+      { text: "Heidi who", score: 89, user: "Bob", handle: "bobboy", link: "https://twitter.com/jack/status/20", date: "March 21, 2006" },
       { text: "Heidi who", score: 89, user: "Bob", handle: "bobboy", link: "https://twitter.com/jack/status/20", date: "March 21, 2006" }
     ];
 
